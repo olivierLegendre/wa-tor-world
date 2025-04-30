c3d6c50c50f62ada52326ee9ed5c3071137f00c0
olivier <olivier.legendre.pro@gmail.com>
2025-04-30 23:02:47 +0200

    add structure files

---
+++ creatures.py
@@ -0,0 +1,21 @@
+class Creature():
+    def __init__(self):
+        pass
+
+
+class Fish(Creature):
+    def __init__(self):
+        pass
+
+
+class Shark(Creature):
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
