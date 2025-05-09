32da896a95289c7c5809e13af9eb5a7026616a90
Aug-Den <augustin.dendievel@gmail.com>
2025-05-09 14:10:02 +0200

    creatures.py modifier pour initialiser l'energie

--- creatures.py
+++ creatures.py
@@ -30,13 +30,14 @@ class Fish(Creature):
 
 class Shark(Creature):
 
-    def __init__(self, coordinate: tuple[int]):
+    def __init__(self, coordinate: tuple[int], initial_energy):
         """Initializer of Fish object
 
         Args:
             coordinate (tuple[int]): coordinate of the creature (x-axis, y-axis)
         """
         super().__init__(coordinate)
+        self.shark_energy = initial_energy
 
     def __repr__(self) -> str:
         """Return a string that represents this object
